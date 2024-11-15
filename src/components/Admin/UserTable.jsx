5cfcde2881b35c13ed0fbc8e8c5907d60780c868
Emaus025 <remiileyva@gmail.com>
2024-11-15 14:23:32 -0600

    tabla

--- src/components/Admin/UserTable.jsx
+++ src/components/Admin/UserTable.jsx
@@ -10,7 +10,8 @@ const UserTable = ({ users, onUpdateUser}) => {
         <tr>
           <th>ID</th>
           <th>Nombre</th>
-          <th>Apellido</th>
+          <th>Apellido Paterno</th>
+          <th>Apellido Materno</th>
           <th>Edad</th>
           <th>Correo</th>
           <th>Fecha Registro</th>
