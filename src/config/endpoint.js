df8550c5e996a2422cb4b2e0ec109bd7beae1f73
Leomont07 <monteroleon52@gmail.com>
2024-12-04 15:04:26 -0600

    Notificaciones

--- src/config/endpoint.js
+++ src/config/endpoint.js
@@ -1,10 +1,11 @@
-const API_BASE_URL = 'http://localhost:3000';
-/*const API_BASE_URL = 'https://back-belehrten.onrender.com';*/
+/*const API_BASE_URL = 'http://localhost:3000';*/
+const API_BASE_URL = 'https://back-belehrten.onrender.com';
 
 export const ENDPOINTS = {
   USERS: `${API_BASE_URL}/users`,
   TESTS: `${API_BASE_URL}/tests`,
-  PLAN: `${API_BASE_URL}/plan`
+  PLAN: `${API_BASE_URL}/plan`,
+  NOTIFICACION: `${API_BASE_URL}/notificacion`
 };
 
 export default API_BASE_URL;
--- src/pages/NotificationsPage.jsx
+++ src/pages/NotificationsPage.jsx
@@ -1,30 +1,55 @@
+import { useEffect, useState } from 'react';
 import NotificationItem from '../components/General/NotificationItem';
 import Header from '../components/Nav/header';
 import Footer from '../components/Nav/footer';
-
-const notificationData = [
-  { type: 'important', content: 'Este contenido podría interesarte' },
-  { type: 'normal', content: 'Este contenido podría interesarte' },
-  { type: 'important', content: 'Este contenido podría interesarte' },
-  { type: 'normal', content: 'Este contenido podría interesarte' },
-];
+import { ENDPOINTS } from '../config/endpoint';
 
 function NotificationsPage() {
+  const [notifications, setNotifications] = useState([]);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    const storedProfileData = JSON.parse(localStorage.getItem('session')) || {};
+    setUserId(storedProfileData.id_usuario || null);
+  }, []);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await fetch(ENDPOINTS.NOTIFICACION + '/notificacion/getNotifications?id_usuario=' + userId);
+
+        // Verificar si la respuesta es exitosa (status 200)
+        if (!response.ok) {
+          throw new Error('Error al obtener las notificaciones');
+        }
+
+        const data = await response.json();
+        setNotifications(data);
+      } catch (error) {
+        console.error('Error al obtener las notificaciones:', error);
+      }
+    };
+
+    fetchNotifications();
+  }, [userId]); 
+
   return (
     <div>
       <Header />
       <section className="flex overflow-hidden flex-col items-center px-20 pt-52 pb-28 text-white bg-white max-md:px-5 max-md:py-24">
         <div className="flex flex-col px-5 pt-5 pb-24 w-full bg-gray-50 rounded-3xl max-w-[928px] min-h-[507px] max-md:max-w-full">
-          <h1 className="text-3xl font-bold text-black max-md:max-w-full">
-            Notificaciones
-          </h1>
-          {notificationData.map((notification, index) => (
-            <NotificationItem
-              key={index}
-              type={notification.type}
-              content={notification.content}
-            />
-          ))}
+          <h1 className="text-3xl font-bold text-black max-md:max-w-full">Notificaciones</h1>
+          {notifications.length > 0 ? (
+            notifications.map((notification, index) => (
+              <NotificationItem
+                key={index}
+                type={notification.type || 'normal'} // Asegúrate de que el backend envíe un campo `type`
+                content={notification.descripcion}
+              />
+            ))
+          ) : (
+            <p className="text-black">No hay notificaciones disponibles.</p>
+          )}
         </div>
       </section>
       <Footer />
